5985c558de986c3c09f72b04ff436d83298d632e
geena-keem <geena.gamm@gmail.com>
2021-07-02 00:22:40 +0900

    ES module

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,51 +1,5 @@
-require('dotenv').config();
+// 이 파일에서만 no-global-assign ESLint 옵션을 비활성화 한다.
+/* eslint-disable no-global-assign */
 
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-const mongoose = require('mongoose');
-
-// 비구조화 할당을 통해 process.env 내부 값에 대한 레퍼런스 만들기
-const { PORT, MONGO_URI } = process.env;
-
-mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((e) => {
-    console.error(e);
-  });
-
-const api = require('./api');
-
-const app = new Koa();
-const router = new Router();
-
-// 라우터 설정
-// router.get('/', (ctx) => {
-//   ctx.body = '홈';
-// });
-// router.get('/about/:name?', (ctx) => {
-//   const { name } = ctx.params;
-//   // name의 존재 유무에 따라 다른 결과 출력
-//   ctx.body = name ? `${name}의 소개` : `소개`;
-// });
-// router.get('/posts', (ctx) => {
-//   const { id } = ctx.query;
-//   // id의 존재 유무에 따라 다른 결과 출력
-//   ctx.body = id ? `포스트 #${id}` : `포스트 아이디가 없습니다.`;
-// });
-router.use('/api', api.routes());
-
-// 라우터 적용 전에 bodyParser 적용
-app.use(bodyParser());
-
-// app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-
-// PORT가 지정되어 있지 않다면 4000을 사용
-const port = PORT || 4000;
-app.listen(port, () => {
-  console.log('Listening to port %d', port);
-});
+require = require('esm')(module /*, options */);
+module.exports = require('./main.js');
