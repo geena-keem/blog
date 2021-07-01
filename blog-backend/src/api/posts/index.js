9478891f40030735e494fff2dbe84723260c0134
geena-keem <geena.gamm@gmail.com>
2021-07-02 02:00:00 +0900

    Request validate

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -13,9 +13,12 @@ const posts = new Router();
 
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
-posts.patch('/:id', postsCtrl.update);
+
+const post = new Router(); // api/posts/:id 경로를 위한 라우터 생성
+post.get('/', postsCtrl.read);
+post.delete('/', postsCtrl.remove);
+post.patch('/', postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 export default posts;
