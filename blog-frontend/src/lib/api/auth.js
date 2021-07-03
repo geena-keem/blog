a90ee56b362880d9979c46df9d726c0e62be31a1
geena-keem <geena.gamm@gmail.com>
2021-07-04 01:24:57 +0900

    redux module

---
+++ blog-frontend/src/lib/api/auth.js
@@ -0,0 +1,13 @@
+// 회원 인증에 필요한 API
+import client from './client';
+
+// 로그인
+export const login = ({ username, password }) =>
+  client.post('/api/auth/login', { username, password });
+
+// 회원가입
+export const register = ({ username, password }) =>
+  client.post('/api/auth/register', { username, password });
+
+// 로그인 상태 확인
+export const check = () => client.get('/api/auth/check');
--- blog-frontend/src/modules/index.js
+++ blog-frontend/src/modules/index.js
@@ -1,8 +1,15 @@
 import { combineReducers } from 'redux';
-import auth from './auth';
+import { allowUnknownOption } from '../../node_modules/commander/typings/index';
+import auth, { authSaga } from './auth';
+import loading from './loading';
 
 const rootReducer = combineReducers({
   auth,
+  loading,
 });
+
+export function* rootSaga() {
+  yield allowUnknownOption([authSaga()]);
+}
 
 export default rootReducer;
