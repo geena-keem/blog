dd35ec8e021b64acd270afb8235fea0985fadd31
geena-keem <geena.gamm@gmail.com>
2021-07-06 01:11:43 +0900

    postviewer page UI + API

---
+++ blog-frontend/src/components/post/PostViewer.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from '../../lib/styles/palette';
+import Responsive from '../common/Responsive';
+
+const PostViewerBlock = styled(Responsive)`
+  margin-top: 4rem;
+  /* background: ${palette.orange[1]}; */
+`;
+
+const PostHead = styled.div`
+  border-bottom: 2px solid ${palette.red[7]};
+  padding-bottom: 3rem;
+  margin-bottom: 3rem;
+
+  h1 {
+    color: ${palette.red[7]};
+    line-height: 1.5;
+    margin: 0;
+  }
+`;
+
+const SubInfo = styled.div`
+  margin-top: 1rem;
+  color: ${palette.red[5]};
+
+  /* span 사이에 가운뎃점 문자 보여주기 */
+  span + span:before {
+    color: ${palette.red[7]};
+    padding-left: 0.25rem;
+    padding-right: 0.25rem;
+    content: '\\B7'; /* 가운뎃점 문자 */
+  }
+`;
+
+const Tags = styled.div`
+  margin-top: 0.5rem;
+  .tag {
+    display: inline-block;
+    color: ${palette.red[7]};
+    text-decoration: none;
+    margin-right: 0.5rem;
+    &:hover {
+      opacity: 0.5;
+    }
+  }
+`;
+
+const PostContent = styled.div`
+  font-size: 1.3125rem;
+  color: ${palette.red[7]};
+`;
+
+const PostViewer = ({ post, error, loading }) => {
+  // 에러 발생 시
+  if (error) {
+    if (error.response && error.response.status === 404) {
+      return <PostViewerBlock>존재하지 않는 포스트입니다.</PostViewerBlock>;
+    }
+    return <PostViewerBlock>오류 발생!</PostViewerBlock>;
+  }
+
+  // 로딩 중이거나 아직 포스트 데이터가 없을 때
+  if (loading || !post) {
+    return null;
+  }
+
+  const { title, body, user, publishedDate, tags } = post;
+
+  return (
+    <PostViewerBlock>
+      <PostHead>
+        <h1>{title}</h1>
+        <SubInfo>
+          <span>
+            <b>{user.username}</b>
+          </span>
+          <span>{new Date(publishedDate).toLocaleDateString()}</span>
+        </SubInfo>
+        <Tags>
+          {tags.map((tag) => (
+            <div className="tag">#{tag}</div>
+          ))}
+        </Tags>
+      </PostHead>
+      <PostContent dangerouslySetInnerHTML={{ __html: body }} />
+    </PostViewerBlock>
+  );
+};
+
+export default PostViewer;
