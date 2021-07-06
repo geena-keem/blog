df60f76bb715bd6c1e3a68afb0ed04eb06de9ba1
geena-keem <geena.gamm@gmail.com>
2021-07-06 17:42:57 +0900

    post delete function

---
+++ blog-frontend/src/components/post/AskRemoveModal.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import AskModal from '../common/AskModal';
+
+const AskRemoveModal = ({ visible, onConfirm, onCancel }) => {
+  return (
+    <AskModal
+      visible={visible}
+      title="POST DELETE"
+      description="Are you sure you want to delete the post?"
+      confirmText="DELETE"
+      onConfirm={onConfirm}
+      onCancel={onCancel}
+    />
+  );
+};
+
+export default AskRemoveModal;
